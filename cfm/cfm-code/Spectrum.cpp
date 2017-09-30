dde3b594e690c30d976f0943b452d93b104e6975
Unknown <tellfei@hotmail.com>
2017-09-29 18:49:28 -0600

    Bugfix

--- cfm/cfm-code/Spectrum.cpp
+++ cfm/cfm-code/Spectrum.cpp
@@ -122,7 +122,10 @@ void Spectrum::postProcess(double perc_thresh, int min_peaks, int max_peaks) {
     // e.g. Take the top 80% of energy (assuming at least 5 peaks),
     // or the highest 30 peaks (whichever comes first)
     if ((total > perc_thresh && count > min_peaks) || count > max_peaks)
-      break;
+    {
+        break;
+    }
+
   }
   peaks.resize(count);
   std::sort(peaks.begin(), peaks.end(), sort_peaks_by_mass);
