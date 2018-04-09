46541645bf488a9c9c2fcec85171dbeb93d7d07c
Fei Wang <tellfei@hotmail.com>
2018-04-09 14:38:19 -0600

    fix debug print

--- cfm/cfm-code/EM.cpp
+++ cfm/cfm-code/EM.cpp
@@ -880,12 +880,10 @@ double EM::computeAndAccumulateGradient(double *grads, int molidx,
                 }
             }
             Q -= nu * log(denom);
-            if(comm->isMaster()
-               && (cfg->random_sampling_threshold < 1.0)){
-                std::cout << "Total: " << fg_map_it->size() <<  " skipped " << skipped << std::endl;
-            }
         }
-
+        if(comm->isMaster() && (cfg->random_sampling_threshold < 1.0)){
+            std::cout << "Total: " << fg_map_it->size() <<  " skipped " << skipped << std::endl;
+        }
     }
 
     return Q;
