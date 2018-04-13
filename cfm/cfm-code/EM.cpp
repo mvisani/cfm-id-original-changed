0bfe0b36548922d98a0c9c5de9194aeec8a22e35
Fei Wang <tellfei@hotmail.com>
2018-04-13 01:43:11 -0600

    [Debug] Random Sampling Crash

--- cfm/cfm-code/EM.cpp
+++ cfm/cfm-code/EM.cpp
@@ -802,17 +802,17 @@ double EM::computeAndAccumulateGradient(double *grads, int molidx,
 
         std::vector<int> not_so_random_selected;
         if (USE_GRAPH_RANDOM_SAMPLING == cfg->ga_sampling_method) {
+            std::cout << moldata.getId() << "...";
             try{
                 moldata.getSampledTransitionIds(not_so_random_selected,
                                                 cfg->ga_graph_sampling_k,
                                                 energy, m_rng,
                                                 m_uniform_dist);
                 std::sort(not_so_random_selected.begin(), not_so_random_selected.end());
-            }catch (std::exception &e)
-            {
+            }catch (std::exception &e) {
                 std::cerr << moldata.getId() << " " << e.what() << std::endl;
             }
-
+            std::cout << "Done" << std::endl;
         }
         //int skipped = 0;
         // Iterate over from_id (i)
