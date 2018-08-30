df4ea63cac22020cdc41275bf11588d660c138c8
Unknown <tellfei@hotmail.com>
2018-08-30 17:39:16 -0600

    adding functional group for fragemnet

---
+++ cfm/cfm-code/Features/FragmentFunctionGroupFeature.cpp
@@ -0,0 +1,45 @@
+/*#########################################################################
+# Mass Spec Prediction and Identification of Metabolites
+#
+# FragmentFingerPrintFeature.h
+#
+# Description: 	Classes for communicating data (e.g. parameters, partial
+#				gradients..etc) during parameter update - see
+param.cpp.
+#
+# Copyright (c) 2013,2018
+# All rights reserved.
+
+# This file is part of the cfm-id project.
+# The contents are covered by the terms of the GNU Lesser General Public
+# License, which is included in the file license.txt, found at the root
+# of the cfm source tree.
+#########################################################################*/
+
+#include "FragmentFunctionGroupFeature.h"
+
+#include <GraphMol/MolOps.h>
+#include <GraphMol/Substruct/SubstructMatch.h>
+
+#include <GraphMol/PartialCharges/GasteigerCharges.h>
+#include <GraphMol/ForceFieldHelpers/MMFF/AtomTyper.h>
+
+
+void FragmentFunctionGroupFeature::compute(FeatureVector &fv, romol_ptr_t precursor_ion) const {
+
+    auto fparams = new RDKit::FragCatParams(FGRPS_PICKLE);
+
+    for(const auto & fg_param :  fparams->getFuncGroups()){
+
+        std::vector<RDKit::MatchVectType> fgpMatches;
+        int has_fg = RDKit::SubstructMatch(*precursor_ion, *fg_param,fgpMatches);
+        if(has_fg){
+            fv.addFeature(1.0);
+        }
+        else{
+            fv.addFeature(0.0);
+        }
+    }
+
+    delete fparams;
+}
