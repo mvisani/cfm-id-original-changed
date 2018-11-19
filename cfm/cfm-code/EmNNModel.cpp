d2131c264d756d6c17c025104faed30b5b0b48b8
Unknown <tellfei@hotmail.com>
2018-11-19 16:34:06 -0700

    Revert "Run faster please"
    This reverts commit 742357b9de75a28fb3f5cd9e2e8c9bc0aa948bb2.

--- cfm/cfm-code/EmNNModel.cpp
+++ cfm/cfm-code/EmNNModel.cpp
@@ -126,8 +126,7 @@ void EmNNModel::computeAndAccumulateGradient(double *grads, int mol_idx, MolData
         //Compute the unweighted gradients
         std::vector<std::vector<double> > unweighted_grads;
         std::set<unsigned int> from_id_used_idxs;
-        nn_param->computeUnweightedGradients(unweighted_grads, from_id_used_idxs, fvs, deltasA, deltasB,
-                                             a_values, record_used_idxs_only);
+        nn_param->computeUnweightedGradients(unweighted_grads, from_id_used_idxs, fvs, deltasA, deltasB, a_values);
 
         //Accumulate the weighted gradients
         std::set<unsigned int>::iterator sit;
