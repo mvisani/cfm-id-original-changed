8cfa06eb7bec2ca7c8683653a42894ead9b250e4
Unknown <tellfei@hotmail.com>
2019-01-22 14:05:19 -0700

    data driven test cases

--- cfm/cfm-boost-test/fraggen_tests/FragGenSimpleBreaksTests.cpp
+++ cfm/cfm-boost-test/fraggen_tests/FragGenSimpleBreaksTests.cpp
@@ -9,6 +9,8 @@
 # Created: November 2012, 2019
 #########################################################################*/
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+namespace bdata = boost::unit_test::data;
 
 #include "MolData.h"
 
@@ -34,21 +36,36 @@ FragmentGraph *getTestGraph(std::string smiles_or_inchi, int ionization_mode) {
     return graph;
 }
 
+void checkFragments(FragmentGraph * graph,
+                    std::vector<std::tuple<int, double, std::string>> & expected,
+                    double tolerance = 1e-5){
+
+    for(const auto & row : expected){
+        int idx = std::get<0>(row);
+        double expected_mass = std::get<1>(row);
+        std::string expected_ion_smiles = std::get<2>(row);
+        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
+        BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
+    }
+}
+
 BOOST_AUTO_TEST_SUITE(FragGenSimpleBreakTests)
 
-    //Expected:
-    //0 61.02840582 CC(O)=[OH+]
-    //1 44.99710569 O=C=[OH+]
-    //2 15.02292652 [CH3+]
-    //3 17.03857658 [CH5+]
-    //4 19.01784114 [OH3+]
-    //5 43.01784114 C#C[OH2+]
-    //6 25.00727645 [C+]#C
-    //7 41.00219107 [C+]#CO
-    //8 59.01275576 [CH+]=C(O)O
+    std::vector<std::tuple<int, double, std::string>> expected_value {
+            {0,61.02840582,"CC(O)=[OH+]"},
+            {1,44.99710569,"O=C=[OH+]"},
+            {2,15.02292652,"[CH3+]"},
+            {3,17.03857658,"[CH5+]"},
+            {4,19.01784114,"[OH3+]"},
+            {5,43.01784114,"C#C[OH2+]"},
+            {6,25.00727645,"[C+]#C"},
+            {7,41.00219107,"[C+]#CO"},
+            {8,59.01275576,"[CH+]=C(O)O"}
+    };
 
-    BOOST_AUTO_TEST_CASE(FragGenTestPositiveESI) {
-
+    BOOST_DATA_TEST_CASE(FragGenTestPositiveESI, bdata::make(expected_value),
+            idx, expected_mass, expected_ion_smiles)
+    {
         std::string smiles_or_inchi = "CC(=O)O";
         int ionization_mode = POSITIVE_ESI_IONIZATION_MODE;
         FragmentGraph *graph = getTestGraph(smiles_or_inchi, ionization_mode);
@@ -56,26 +73,14 @@ BOOST_AUTO_TEST_SUITE(FragGenSimpleBreakTests)
         double tolerance =  1e-6;
         BOOST_CHECK_EQUAL(graph->getNumFragments(), 9);
 
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(0)->getMass(), 61.02840582, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(1)->getMass(), 44.99710569, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(2)->getMass(), 15.02292652, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(3)->getMass(), 17.03857658, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(4)->getMass(), 19.01784114, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(5)->getMass(), 43.01784114, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(6)->getMass(), 25.00727645, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(7)->getMass(), 41.00219107, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(8)->getMass(), 59.01275576, tolerance);
-        delete graph;
+        std::cout << idx << expected_mass << expected_ion_smiles << std::endl;
+        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
+        BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
     }
 
     BOOST_AUTO_TEST_CASE(FragGenTestNegativeESI) {
 
-        //Expected:
-        // 0 59.01385292 CC(=O)O"
-        // 1 15.02402368 [CH3-]"
-        // 2 17.00328823 [OH-]"
-        // 3 41.00328823 C#C[O-]"
-        double tolerance =  1e-6;
+        double tolerance =  1e-5;
 
         std::string smiles_or_inchi = "CC(=O)O";
         int ionization_mode = NEGATIVE_ESI_IONIZATION_MODE;
@@ -83,32 +88,21 @@ BOOST_AUTO_TEST_SUITE(FragGenSimpleBreakTests)
         FragmentGraph *graph = getTestGraph(smiles_or_inchi, ionization_mode);
 
         BOOST_CHECK_EQUAL(graph->getNumFragments(), 4);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(0)->getMass(), 59.01385292, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(1)->getMass(), 15.02402368, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(2)->getMass(), 17.00328823, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(3)->getMass(), 41.00328823, tolerance);
+        std::vector<std::tuple<int, double, std::string>> expected;
+
+        // Expected
+        expected.push_back(std::make_tuple(0,59.01385292,"CC(=O)[O-]"));
+        expected.push_back(std::make_tuple(1,15.02402368,"[CH3-]"));
+        expected.push_back(std::make_tuple(2,17.00328823,"[OH-]"));
+        expected.push_back(std::make_tuple(3,41.00328823 ,"C#C[O-]"));
+
+        checkFragments(graph, expected, tolerance);
 
         delete graph;
     }
 
     BOOST_AUTO_TEST_CASE(FragGenTestPositiveEI) {
 
-        //Expected:
-        //0 60.02058079 CC(=[O+])O
-        //1 43.98928066 O=C=[O+]
-        //2 44.99710569 O=C=[OH+]
-        //3 15.02292652 [CH3+]
-        //4 16.03075155 [CH4+]
-        //5 18.0100161 [OH2+]
-        //6 19.01784114 [OH3+]
-        //7 43.01784114 C#C[OH2+]
-        //8 25.00727645 [C+]#C
-        //9 41.00219107 [C+]#CO
-        //10 42.0100161 C#C[OH+]
-        //11 23.99945142 [C]#[C+]
-        //12 39.99436604 [C+]#C[O]
-        //13 58.00493072 [CH+]=C([O])O
-        //14 59.01275576 [CH+]=C(O)O
 
         double tolerance =  1e-6;
 
@@ -119,21 +113,23 @@ BOOST_AUTO_TEST_SUITE(FragGenSimpleBreakTests)
 
         BOOST_CHECK_EQUAL(graph->getNumFragments(), 15);
 
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(0)->getMass(), 60.02058079, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(1)->getMass(), 43.98928066, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(2)->getMass(), 44.99710569, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(3)->getMass(), 15.02292652, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(4)->getMass(), 16.03075155, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(5)->getMass(), 18.0100161, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(6)->getMass(), 19.01784114 , tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(7)->getMass(), 43.01784114, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(8)->getMass(), 25.00727645, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(9)->getMass(), 41.00219107, tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(10)->getMass(), 42.0100161 , tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(11)->getMass(), 23.99945142 , tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(12)->getMass(), 39.99436604 , tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(13)->getMass(), 58.00493072 , tolerance);
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(14)->getMass(), 59.01275576 , tolerance);
+        std::vector<std::tuple<int, double, std::string>> expected;
+        expected.push_back(std::make_tuple(0,60.02058079,"CC(=[O+])O"));
+        expected.push_back(std::make_tuple(1,43.98928066,"O=C=[O+]"));
+        expected.push_back(std::make_tuple(2,44.99710569,"O=C=[OH+]"));
+        expected.push_back(std::make_tuple(3,15.02292652,"[CH3+]"));
+        expected.push_back(std::make_tuple(4,16.03075155,"[CH4+]"));
+        expected.push_back(std::make_tuple(5,18.0100161,"[OH2+]"));
+        expected.push_back(std::make_tuple(6,19.01784114,"[OH3+]"));
+        expected.push_back(std::make_tuple(7,43.01784114,"C#C[OH2+]"));
+        expected.push_back(std::make_tuple(8,25.00727645,"[C+]#C"));
+        expected.push_back(std::make_tuple(9,41.00219107,"[C+]#CO"));
+        expected.push_back(std::make_tuple(10,42.0100161,"C#C[OH+]"));
+        expected.push_back(std::make_tuple(11,23.99945142,"[C]#[C+]"));
+        expected.push_back(std::make_tuple(12,39.99436604,"[C+]#C[O]"));
+        expected.push_back(std::make_tuple(13,58.00493072,"[CH+]=C([O])O"));
+        expected.push_back(std::make_tuple(14,59.01275576,"[CH+]=C(O)O"));
+        checkFragments(graph, expected, tolerance);
 
         delete graph;
     }
