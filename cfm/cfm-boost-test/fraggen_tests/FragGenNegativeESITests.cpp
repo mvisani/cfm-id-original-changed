c7dae9f5535b6b138ae2e2822d9b04d81df47db8
Unknown <tellfei@hotmail.com>
2019-01-24 22:01:41 -0700

    Add Negative ESI Ring Tests

--- cfm/cfm-boost-test/fraggen_tests/FragGenNegativeESITests.cpp
+++ cfm/cfm-boost-test/fraggen_tests/FragGenNegativeESITests.cpp
@@ -80,3 +80,78 @@ BOOST_FIXTURE_TEST_SUITE(FragGenTestNegativeESIRing, NegativeESIRingFixture)
         BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
     }
 BOOST_AUTO_TEST_SUITE_END()
+
+struct NegavtiveESIRingFixture{
+    NegavtiveESIRingFixture() {
+        graph = getTestGraph("C1=CN=CN=C1", NEGATIVE_ESI_IONIZATION_MODE, true);
+    }
+    ~NegavtiveESIRingFixture() { delete graph; };
+    FragmentGraph *graph;
+};
+
+BOOST_FIXTURE_TEST_SUITE(FragGenTestNegavtiveESIRing, NegavtiveESIRingFixture)
+
+    std::vector<std::tuple<int, double, std::string>> expected_value {
+            {0,79.03017168,"[c-]1cncnc1"},
+            {2,53.01452161,"[CH-]=NC#N"},
+            {3,26.00362258,"[C-]#N"},
+            {4,25.00837361,"[C-]#C"},
+            {5,27.02402368,"[CH-]=C"},
+            {6,28.01927264,"C=[N-]"},
+            {7,50.00362258,"[C-]#CC#N"},
+            {8,52.01927264,"C=[C-]C#N"},
+            {10,52.01927264,"[C-]#CN=C"},
+            {11,77.01452161,"[CH-]=NC#CC#N"},
+    };
+
+    BOOST_AUTO_TEST_CASE(NumOfFragmentTest){
+        BOOST_CHECK_EQUAL(graph->getNumFragments(), 12);
+    }
+
+    BOOST_DATA_TEST_CASE(FragmentTests, bdata::make(expected_value),
+                         idx, expected_mass, expected_ion_smiles)
+    {
+        double tolerance =  1e-6;
+
+        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
+        BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
+    }
+
+    // Ring Breaks produced by old cfmid
+    // let us check if we cover those cases
+    std::vector<std::tuple<int, double, std::string>> expected_ring_breaks {
+            {0,79.03017168,"[c-]1cncnc1"},
+            {10,52.01927264,"[C-]#CN=C"},
+            {3,26.00362258,"[C-]#N"},
+            {4,25.00837361,"[C-]#C"},
+            {6,28.01927264,"C=[N-]"},
+            {5,27.02402368,"[CH-]=C"},
+            {2,53.01452161,"[CH-]=NC#N"},
+            {8,52.01927264,"C=[C-]C#N"},
+            {7,50.00362258,"[C-]#CC#N"},
+    };
+
+    BOOST_DATA_TEST_CASE(RingBreakFragmentTests, bdata::make(expected_ring_breaks),
+                         idx, expected_mass, expected_ion_smiles)
+    {
+        double tolerance =  1e-6;
+
+        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
+    }
+
+    std::vector<std::tuple<int, double, std::string>> expected_intermediate_fragments {
+            {1,79.03017168,"C=C=C=NC=[N-]"},
+            {9,79.03017168,"CN=C=C=C=[N-]"}
+
+    };
+    BOOST_DATA_TEST_CASE(IntermediateFragmentTests, bdata::make(expected_intermediate_fragments),
+                         idx, expected_mass, expected_ion_smiles)
+    {
+        double tolerance =  1e-6;
+
+        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
+        BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
+        BOOST_CHECK(graph->getFragmentAtIdx(idx)->isIntermediate());
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
