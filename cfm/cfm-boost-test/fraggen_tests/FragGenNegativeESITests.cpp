604e1ec050ce4f866791a5e4bf53337ac8375eb8
Unknown <tellfei@hotmail.com>
2019-01-29 14:33:13 -0700

    Fixing Neagtive Rings Test

--- cfm/cfm-boost-test/fraggen_tests/FragGenNegativeESITests.cpp
+++ cfm/cfm-boost-test/fraggen_tests/FragGenNegativeESITests.cpp
@@ -58,41 +58,6 @@ BOOST_FIXTURE_TEST_SUITE(FragGenTestNegativeESIRing, NegativeESIRingFixture)
 
     std::vector<std::tuple<int, double, std::string>> expected_value {
             {0,79.03017168,"[c-]1cncnc1"},
-            {1,52.01927264,"[C-]#CN=C"},
-            {2,26.00362258,"[C-]#N"},
-            {3,25.00837361,"[C-]#C"},
-            {4,28.01927264,"C=[N-]"},
-            {5,27.02402368,"[CH-]=C"},
-            {6,53.01452161,"[CH-]=NC#N"},
-            {7,52.01927264,"C=[C-]C#N"},
-            {8,50.00362258,"[C-]#CC#N"}
-    };
-
-    BOOST_AUTO_TEST_CASE(NumOfFragmentTest){
-        BOOST_CHECK_EQUAL(graph->getNumFragments(), 8);
-    }
-
-    BOOST_DATA_TEST_CASE(FragmentTests, bdata::make(expected_value),
-                         idx, expected_mass, expected_ion_smiles)
-    {
-        double tolerance =  1e-6;
-        BOOST_CHECK_CLOSE_FRACTION(graph->getFragmentAtIdx(idx)->getMass(), expected_mass, tolerance);
-        BOOST_CHECK_EQUAL(*(graph->getFragmentAtIdx(idx)->getIonSmiles()), expected_ion_smiles);
-    }
-BOOST_AUTO_TEST_SUITE_END()
-
-struct NegavtiveESIRingFixture{
-    NegavtiveESIRingFixture() {
-        graph = getTestGraph("C1=CN=CN=C1", NEGATIVE_ESI_IONIZATION_MODE, true);
-    }
-    ~NegavtiveESIRingFixture() { delete graph; };
-    FragmentGraph *graph;
-};
-
-BOOST_FIXTURE_TEST_SUITE(FragGenTestNegavtiveESIRing, NegavtiveESIRingFixture)
-
-    std::vector<std::tuple<int, double, std::string>> expected_value {
-            {0,79.03017168,"[c-]1cncnc1"},
             {2,53.01452161,"[CH-]=NC#N"},
             {3,26.00362258,"[C-]#N"},
             {4,25.00837361,"[C-]#C"},
