4629ddd12c658e39c869241da06b656d2a2bfe1d
Fei Wang <tellfei@hotmail.com>
2019-08-21 19:53:24 -0600

    change stats-compute

--- cfm/cfm-code/Spectrum.cpp
+++ cfm/cfm-code/Spectrum.cpp
@@ -117,7 +117,7 @@ void Spectrum::postProcess(double perc_thresh, int min_peaks, int max_peaks, dou
         count++;
         // e.g. Take the top 80% of energy (assuming at least 5 peaks),
         // or the highest 30 peaks (whichever comes first)
-        if ((total > perc_thresh && count > min_peaks) || count >= max_peaks || it->intensity < min_intensity) {
+        if ((total > perc_thresh && count >= min_peaks) || count >= max_peaks || it->intensity < min_intensity) {
             break;
         }
     }
