0aea83fbce4992a03fb0fbdbb71d509c8d032dd3
Fei Wang <tellfei@hotmail.com>
2019-08-21 21:03:55 -0600

    remove lower bond when postprocess

--- cfm/compute-stats/main.cpp
+++ cfm/compute-stats/main.cpp
@@ -330,7 +330,7 @@ int main(int argc, char *argv[]) {
                 mit->quantiseMeasuredSpectra(quantise_spectra_dec_pl);
             }
             if (apply_cutoffs)
-                mit->postprocessPredictedSpectra(cumulative_intensity_thresh, 5, 30, min_intensity);
+                mit->postprocessPredictedSpectra(cumulative_intensity_thresh, 0, 30, min_intensity);
             else
                 mit->postprocessPredictedSpectra(cumulative_intensity_thresh, 0, 1000000, min_intensity);
 
