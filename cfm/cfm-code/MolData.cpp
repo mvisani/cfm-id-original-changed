8e9a69f8101aa87509c54c9f6f8a43c806f3ae28
Fei Wang <tellfei@hotmail.coim>
2020-06-24 14:36:29 -0600

    Edited Specturm Header Style

--- cfm/cfm-code/MolData.cpp
+++ cfm/cfm-code/MolData.cpp
@@ -736,7 +736,7 @@ void MolData::outputSpectra(std::ostream &out, const char *spec_type,
             default:
                 break;
         }
-        out << "#" << spectra_str << " PREDICTED BY " << APP_STRING << " " << PROJECT_VER << std::endl;
+        out << "#" << spectra_str << std::endl << "#PREDICTED BY " << APP_STRING << " " << PROJECT_VER << std::endl;
     }
     std::vector<Spectrum>::iterator it = spectra_to_output->begin();
     for (int energy = 0; it != spectra_to_output->end(); ++it, energy++) {
