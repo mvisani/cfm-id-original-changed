17ef05b7cc387d18d3d6ec7d9017b5e3cd22dff3
Fei Wang <tellfei@hotmail.com>
2021-02-22 14:53:48 -0700

    Add Config for allow intermidate peaks

--- cfm/cfm-code/MolData.cpp
+++ cfm/cfm-code/MolData.cpp
@@ -350,10 +350,10 @@ void MolData::computeLogTransitionProbabilities() {
         // Set the persistence log probabilities
         int offset = fg->getNumTransitions();
         for (unsigned int i = 0; i < fg->getNumFragments(); i++){
-            //if(fg->getFragmentAtIdx(i)->isIntermediate())
-            //    log_probs[energy][offset + i] = -100000000;
-            //else
-            log_probs[energy][offset + i] = -denom_cache[i];
+            if(fg->getFragmentAtIdx(i)->isIntermediate() && cfg->allow_intermediate_peak)
+                log_probs[energy][offset + i] = -100000000;
+            else
+                log_probs[energy][offset + i] = -denom_cache[i];
         }
     }
 }
