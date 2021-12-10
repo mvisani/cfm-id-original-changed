5d5c84b3472e31049ea8d31f830530977a8979fd
Fei Wang <tellfei@hotmail.com>
2021-12-10 09:54:14 -0700

    Update

--- cfm/cfm-code/Version.h
+++ cfm/cfm-code/Version.h
@@ -5,9 +5,9 @@
 #define INCLUDE_VERSION
 
 #define PROJECT_NAME "cfm"
-#define PROJECT_VER  "4.1.2"
+#define PROJECT_VER  "4.2.0"
 #define PROJECT_VER_MAJOR "4"
-#define PROJECT_VER_MINOR "1"
-#define PTOJECT_VER_PATCH "2"
+#define PROJECT_VER_MINOR "2"
+#define PTOJECT_VER_PATCH "0"
 
 #endif // INCLUDE_VERSION
